9627b3062ee33f6773fb83a6ca3b6608e6864792
George Gilmer <george.gilmer@gmail.com>
2017-09-18 13:26:13 -0400

    separated constants modules and classes

---
+++ category.rb
@@ -0,0 +1,19 @@
+module Category
+  GOOGLE_PRODUCT_CATEGORY = 'Furniture > Baby & Toddler Furniture > Crib & Toddler Bed Accessories'
+
+  FEED_HEADER = [
+  'id',
+  'title',
+  'description',
+  'link',
+  'image_link',
+  'availability',
+  'price',
+  'google_product_category',
+  'product_type',
+  'brand',
+  'mpn',
+  'condition',
+  'gtin']
+
+end
